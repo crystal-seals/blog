9b9e6ec6d089300d6772fc9b0e20378eb6f4f6f6
TroyMaeder <troy.maeder@gmail.com>
2016-06-08 15:14:54 +0100

    Add button function

--- blogs.js
+++ blogs.js
@@ -1,44 +1,28 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>title</title>
-  <link rel="stylesheet" href="style.css" type="text/css">
-  </head>
-  <body>
-  	<div class="heading">
-  		<h3>About</h3>
-  	</div>
-  	<div class="main-bio">
-	  	<p>Techies is a portrait project focused on sharing stories of tech employees in Silicon Valley.
-	  	</p>
-	  	<p>
-		We cover subjects who tend to be underrepresented in the greater tech narrative. This includes (but is not limited to) women, people of color, folks over 50, LGBT, working parents, disabled, etc.
-		</p>
-		<p>
-		The project has two main goals: to show the outside world a more comprehensive picture of people who work in tech, and to bring a bit of attention to folks in the industry whose stories have never been heard, considered or celebrated. We believe storytelling is a powerful tool for social impact and positive change.
-		</p>
-	</div>
-	
-	<div class="bio-pic">
-		<img src="http://placekitten.com/1500/1000">
-	</div>
-	<hr class="style-one">
-	<div class="team-name">
-		<p>NAME OF TEAM</p>
-	</div>
-	<hr class="style-one">
-	<div class="team">
-		<h3>TEAM</h3>
-	</div>
-	<div class="team-p">
-		<p>Techies was conceived in January 2016 by San Francisco-based photographer Helena Price.<p> 
+var isItClicked = true;
 
-		<p>It was brought to life over the course of three months with the help of designer Alonzo Felix and developer Martha Schumann.
-		</p>
+function removeHighlight(){
+	var menuItems = document.querySelectorAll('.navbarcontainer div');
+	for (var i = 0; i < 3; i++) {
+		menuItems[i].style.backgroundColor = '#FFFFF0';
+	}
+}
 
-		<p>Special thanks to Brian Lam, Michelle Morrison, Bianca L. St.Louis, Stewart Scott Curran, Liz Gershman, Anne Barrington, Kate Stern, Laura Brunow Miner, Maggie Bignell, Matt Buchanan, Sylvia Killingsworth, Josh Puckett, and Fiona Tang, and thank you to all of our contributors for being so generous with their stories and time over the course of the project
-		</p>
-	</div>
-  </body>
-</html>
+function firstFunc(event) {
+	removeHighlight();
+	// event.target
+	document.getElementById('profile').style.backgroundColor = "#611C35";
+}
+
+function secondFunc() {
+	removeHighlight();
+	document.getElementById('about').style.backgroundColor = "#611C35";
+}
+
+function thirdFunc() {
+	removeHighlight();
+	document.getElementById('projects').style.backgroundColor = "#611C35";
+}
+
+// var menu = document.querySelectorAll(...);
+
+// menu.addEventListener('click', firstFunc);
