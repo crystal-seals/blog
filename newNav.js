4ec0b7246a8cd64ca0a549f9c555cad4580e4503
TroyMaeder <troy.maeder@gmail.com>
2016-06-08 16:22:25 +0100

    Adding navbar

---
+++ newNav.js
@@ -0,0 +1,30 @@
+var isItClicked = true;
+
+function removeHighlight(){
+	var menuItems = document.querySelectorAll('.navbarcontainer div');
+	for (var i = 0; i < 3; i++) {
+		//menuItems[i].style.backgroundColor = '#FFFFF0';
+		menuItems[i].className = 'button';
+	}
+}
+
+function firstFunc(event) {
+	removeHighlight();
+	// event.target
+	//document.getElementById('profile').style.backgroundColor = "#611C35";
+	document.getElementById('profile').className = 'button highlighted';
+}
+
+function secondFunc() {
+	removeHighlight();
+	document.getElementById('about').className = 'button highlighted';
+}
+
+function thirdFunc() {
+	removeHighlight();
+	document.getElementById('projects').className = 'button highlighted';
+}
+
+// var menu = document.querySelectorAll(...);
+
+// menu.addEventListener('click', firstFunc);
